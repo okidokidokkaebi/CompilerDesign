9a962d29788828f6e621123d75123f94b4bc6885
Nikola <ulsxl@student.kit.edu>
2025-05-18 19:29:37 +0200

    remove assembly comments

--- src/main/java/edu/kit/kastel/vads/compiler/backend/aasm/CodeGenerator.java
+++ src/main/java/edu/kit/kastel/vads/compiler/backend/aasm/CodeGenerator.java
@@ -31,9 +31,7 @@ public class CodeGenerator {
                 .text
                 main:
                 call _main
-                ; move the return value into the first argument for the syscall
                 movq %rax, %rdi
-                ; move the exit syscall number into rax
                 movq $0x3C, %rax
                 syscall
                 """);
