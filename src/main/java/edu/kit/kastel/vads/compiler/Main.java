fb99ca1eac401c784f579e2c219a38448493569c
Nikola <ulsxl@student.kit.edu>
2025-05-19 22:29:02 +0200

    use all available physical registers

--- src/main/java/edu/kit/kastel/vads/compiler/Main.java
+++ src/main/java/edu/kit/kastel/vads/compiler/Main.java
@@ -100,14 +100,21 @@ public class Main {
         System.out.println(virtualRegisters);
 
         String[] registers = {
-                "%r8",
-                "%r9",
-                "%r10",
-                "%r11",
-                "%r12",
-                "%r13",
-                "%r14",
-                "%r15",
+            "%rbx",
+            "%rcx",
+            "%rdx",
+            "%rsi",
+            "%rdi",
+            "%rsp",
+            "%rbp",
+            "%r8",
+            "%r9",
+            "%r10",
+            "%r11",
+            "%r12",
+            "%r13",
+            "%r14",
+            "%r15",
         };
 
         List<Map.Entry<String, Integer>> active = new ArrayList<>();
