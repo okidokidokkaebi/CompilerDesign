563f14bd5c23231645c88217f7766c0edb60b0fd
Nikola <ulsxl@student.kit.edu>
2025-06-02 11:16:32 +0200

    push debug file

--- src/test/resources/test.c
+++ src/test/resources/test.c
@@ -1,22 +1,24 @@
 int main() {
   int a;
   int b;
-  int c = 63;
-  int d = 49;
-  int e = 61;
-  int f = 96;
-  int g = 51;
-  int i = 10;
-  int j = 31;
-  int k = 31;
-  int m = 30;
-  int q = 44;
-  int r = 21;
+  int c = 11;
+  int d = 22;
+  int e = 33;
+  int f = 44;
+
+  int g = 55;
+  int i = 66;
+  int j = 77;
+  int k = 88;
+
+  int l = 99;
+  int q = 111;
+  int r = 222;
+  int s = 333;
 
           /*    Epic tower of simple arithmetic operations - but reduced so I can actually read the program... */
-                           a = c%d + e/f/g - q/r;
+                           a = (e/f) + (q/r) + (c/d);
+                          b = a + (s/g) + (i/j) + (k/l);
 
-                          b = c/d - e/g - i/j + k/m;
-
-  return c;
+  return b;
 }
